0f7ba48aabea6e1557d0b9d243ad3adff8b31d91
Farriq muwaffaq <farriqmuwaffaq100@gmail.com>
2024-05-23 17:20:36 +0700

    fix: status ijin

--- resources/views/perijinan/guru/status.blade.php
+++ resources/views/perijinan/guru/status.blade.php
@@ -3,7 +3,7 @@
         Diijinkan <i class="bx bx-check"></i>
     </span>
 @else
-    <span class="badge bg-success">
+    <span class="badge bg-danger">
         Belum Diijinkan <i class="bx bx-x"></i>
     </span>
 @endif
